98215d5068ee8ec3a6ef74c192c41055dbf8a42b
DavidDelgado <universovinny@gmail.com>
2023-10-11 18:06:52 -0500

    Initial commit

---
+++ app/src/main/java/com/fabiansuarez/mywallet/Splash_Activity.java
@@ -0,0 +1,28 @@
+package com.fabiansuarez.mywallet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class Splash_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent myIntent = new Intent(Splash_Activity.this, CategoryActivity.class);
+                startActivity(myIntent);
+                finish();
+            }
+        }, 5000);
+    }
+}
